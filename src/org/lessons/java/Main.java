2f76d5da746a7cf9a88da6d7596d85e0296b2880
Alaikum <carangiadamo@gmail.com>
2023-02-08 15:13:08 +0100

    Milestone 3

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) throws SQLException {
@@ -12,6 +13,7 @@ public class Main {
 		String url = "jdbc:mysql://localhost:3306/nations";
 		String user = "root";
 		String password = "root";
+		Scanner s = new Scanner(System.in);
 
 		try (Connection con = DriverManager.getConnection(url, user, password);) {
 			System.out.println("Connessione Effettuata con successo");
@@ -19,16 +21,22 @@ public class Main {
 			// SCRIVO QUERY
 			String sql = "SELECT c.name ,c.country_id, r.name , c2.name  FROM countries c \r\n"
 					+ "JOIN regions r on c.region_id =r.region_id \r\n"
-					+ "JOIN continents c2 on r.continent_id =c2.continent_id \r\n" + "ORDER BY c.name ";
+					+ "JOIN continents c2 on r.continent_id =c2.continent_id \r\n" + "WHERE c.name like ? \r\n"
+					+ "ORDER BY c.name ";
 			try (PreparedStatement ps = con.prepareStatement(sql)) {
+				System.out.println("Fai una ricerca per nome o lettere presenti nel nome della Nazione: ");
+				
+				ps.setString(1, "%"+s.nextLine()+"%");
+
 				try (ResultSet rs = ps.executeQuery()) {
 					while (rs.next()) {
-						System.out.println("Nation: "+rs.getString(1) + " Id: " + rs.getString(2) + " Region: " + rs.getString(3) + " Continent: "
-								+ rs.getString(4));
+						System.out.println("Nation: " + rs.getString(1) + " Id: " + rs.getString(2) + " Region: "
+								+ rs.getString(3) + " Continent: " + rs.getString(4));
 					}
 				}
 			}
 		}
+		s.close();
 	}
 
 }
